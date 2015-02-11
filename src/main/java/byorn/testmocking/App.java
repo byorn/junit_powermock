7e0213f0fdf1159ca07f80014eb45e2221beb3f3
Byorn De Silva <byorn.desilva@knowit.se>
2015-02-11 17:42:43 +0530

    init

---
+++ src/main/java/byorn/testmocking/App.java
@@ -0,0 +1,13 @@
+package byorn.testmocking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
